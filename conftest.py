462a94ca384d12967b657f6afeeb3fd5a1ae7bbd
Sen <senyang95@gmail.com>
2022-03-01 11:07:19 +0800

    change dir

--- conftest.py
+++ conftest.py
@@ -4,12 +4,5 @@ Created on Sat May 16 15:21:36 2020
 
 @author: Sen
 """
-import pytest
 
-def pytest_addoption(parser):
-    parser.addoption('--host', action='store', help='host of db')
-    parser.addoption('--port', action='store', default='8888', help='port of db')
-    
-@pytest.fixture
-def config(request):
-    return request.config
+pytest_plugins = ("test.plugin",)
